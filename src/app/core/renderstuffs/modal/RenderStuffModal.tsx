7984ebae8363a3f3f570c23a4ff03330dee85214
kelvin <kelvinices@gmail.com>
2022-10-17 18:57:22 +0700

    render stuff

---
+++ src/app/core/renderstuffs/modal/RenderStuffModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RenderStuffModal = () => {
+  return (
+    <div>
+        <iframe width="1200" height="700" title="someTitle" scrolling="no" src="https://competition.binus.ac.id/vr/i/i004.jpg"></iframe>
+    </div>
+  )
+}
