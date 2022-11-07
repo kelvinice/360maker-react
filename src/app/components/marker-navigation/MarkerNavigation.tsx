e28b4f579628d551e2f2bd483021ee331648b36b
kelvin <kelvinices@gmail.com>
2022-11-07 15:14:20 +0700

    marker dropdown

--- src/app/components/marker-navigation/MarkerNavigation.tsx
+++ src/app/components/marker-navigation/MarkerNavigation.tsx
@@ -1,24 +1,66 @@
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useCallback} from 'react';
 import Draggable from "react-draggable";
-import {Chat, Close, Image, Mouse, OpenWith, PinDrop, Settings, Videocam, Delete} from '@material-ui/icons'
+import {Chat, Close, Image, Mouse, OpenWith, Settings, Videocam, Delete, PinDropSharp, LocationSearching} from '@material-ui/icons'
 import {useMenu} from "../../providers/MenuProvider";
 import {useAtom} from "jotai";
 import {mouseStateAtom} from "../../atoms/DataAtom";
 import clsx from "clsx";
 import {MouseState} from "../../constants/MouseState";
+import {MDBDropdownItem, MDBDropdownMenu, MDBDropdownToggle, MDBDropdown, MDBBtn} from "mdb-react-ui-kit";
+import {useAtomCallback} from "jotai/utils";
 
-const MouseStateButton = (props: { targetMouseState: MouseState, color : string, children: ReactNode }) => {
+
+const MouseStateButton = (props: { targetMouseState: MouseState, color : string, children: ReactNode, text?: string, className?: string }) => {
     const [mouseState, setMouseState] = useAtom(mouseStateAtom);
-    return <button className={clsx({
+    return (
+        <div className={clsx({
             "active": props.targetMouseState === mouseState,
-        }, `btn btn-sm btn-${props.color}`)} onClick={()=>setMouseState(props.targetMouseState)}>
-        {props.children}
-    </button>;
+        }, `btn btn-sm btn-${props.color} d-flex align-item-center ${props.className || ""}`)} onClick={()=>setMouseState(props.targetMouseState)}>
+            <div className="d-flex align-items-center ">
+                {props.children}
+                {props.text && <span className="ms-2 fw-bold">{props.text}</span>}
+            </div>
+
+        </div>
+    )
+
 }
 
 const MarkerNavigation = () => {
     const {setMarkerNavigationOpen} = useMenu();
     const nodeRef = React.useRef(null);
+    const [mouseState, setMouseState] = useAtom(mouseStateAtom);
+
+    const markers = [
+        {
+            icon: <LocationSearching />,
+            text: "Place Marker",
+            targetMouseState: MouseState.MarkerPlace,
+        },
+        {
+            icon: <Image />,
+            text: "Image Marker",
+            targetMouseState: MouseState.MarkerImage,
+        },
+        {
+            icon: <Videocam />,
+            text: "Video Marker",
+            targetMouseState: MouseState.MarkerVideo,
+        },
+        {
+            icon: <Chat />,
+            text: "Text Marker",
+            targetMouseState: MouseState.MarkerDescription,
+        }
+    ]
+
+    const getMouseState = useAtomCallback(useCallback((get, set) => {
+        return get(mouseStateAtom);
+    }, []));
+
+    console.log(getMouseState());
+
+    const currentMarker = markers.find(marker => marker.targetMouseState === getMouseState());
 
     return (
         <Draggable handle=".btn-drag" nodeRef={nodeRef}>
@@ -34,10 +76,38 @@ const MarkerNavigation = () => {
                     <MouseStateButton color="info" targetMouseState={MouseState.Cursor} children={<Mouse/>}/>
                     <MouseStateButton color="info" targetMouseState={MouseState.Setting} children={<Settings/>}/>
                     <MouseStateButton color="info" targetMouseState={MouseState.Delete} children={<Delete/>}/>
-                    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerPlace} children={<PinDrop/>}/>
-                    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerImage} children={<Image />}/>
-                    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerVideo} children={<Videocam />}/>
-                    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerDescription} children={<Chat />}/>
+
+                    <MDBDropdown group>
+                        <MDBDropdownToggle className={clsx({
+                            "active": currentMarker !== undefined,
+                        }, "btn btn-primary")}>
+                            {
+                                !currentMarker ? <PinDropSharp /> : currentMarker.icon
+                            }
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu style={{minWidth:0}} >
+                            {
+                                markers.map((marker, index) => (
+                                    <MDBDropdownItem link key={index} className="flat-a">
+                                        <MouseStateButton text={marker.text} color="primary" targetMouseState={marker.targetMouseState} children={marker.icon}
+                                            className="m-2"
+                                        />
+                                    </MDBDropdownItem>
+                                ))
+                            }
+
+                            {/*<MDBDropdownItem link>*/}
+                            {/*    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerImage} children={<Image />}/>*/}
+                            {/*</MDBDropdownItem>*/}
+                            {/*<MDBDropdownItem link>*/}
+                            {/*    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerVideo} children={<Videocam />}/>*/}
+                            {/*</MDBDropdownItem>*/}
+                            {/*<MDBDropdownItem link>*/}
+                            {/*    <MouseStateButton color="primary" targetMouseState={MouseState.MarkerDescription} children={<Chat />}/>*/}
+                            {/*</MDBDropdownItem>*/}
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+
 
                     <div className="border border-dark border-1 h-auto"/>
 
